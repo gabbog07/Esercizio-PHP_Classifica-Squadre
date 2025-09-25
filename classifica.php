255963bbbf9a129c6e30bd7adba90b2933657a3b
gabbo <gabri152007@gmail.com>
2025-09-25 16:11:12 +0200

    fine

--- classifica.php
+++ classifica.php
@@ -1,24 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Raccolta dati squadra 1
     $nome1 = $_POST["nome1"];
     $vinte1 = (int)$_POST["vinte1"];
     $pareggiate1 = (int)$_POST["pareggiate1"];
-    $perse1 = (int)$_POST["perse1"];
-    $partite1 = $vinte1 + $pareggiate1 + $perse1;
+    $sconfitte1 = (int)$_POST["sconfitte1"];
+    $partite1 = $vinte1 + $pareggiate1 + $sconfitte1;
     $punti1 = $vinte1 * 3 + $pareggiate1;
 
-    // Raccolta dati squadra 2
     $nome2 = $_POST["nome2"];
     $vinte2 = (int)$_POST["vinte2"];
     $pareggiate2 = (int)$_POST["pareggiate2"];
-    $perse2 = (int)$_POST["perse2"];
-    $partite2 = $vinte2 + $pareggiate2 + $perse2;
+    $sconfitte2 = (int)$_POST["sconfitte2"];
+    $partite2 = $vinte2 + $pareggiate2 + $sconfitte2;
     $punti2 = $vinte2 * 3 + $pareggiate2;
 
     echo "<h2>Classifica</h2>";
 
-    // Ordina le squadre per punti
     if ($punti1 > $punti2) {
         echo "<p>$nome1 = $punti1</p>";
         echo "<p>$nome2 = $punti2</p>";
@@ -27,7 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p>$nome1 = $punti1</p>";
     }
 
-    // Controllo sul numero di partite
     if ($partite1 != $partite2) {
         echo "<p><b>Le due squadre hanno giocato un numero di partite diverso.</b></p>";
     }
